89dc532048f586401cb76e97293c7a5387db6915
mayadennise2001 <maya_ramirez@davincischools.org>
2019-01-28 14:22:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+// "el" stands for the "element" that was just clicked
+function addClass(el){
+	el.classList.add("opaque");
+
+}
+
+// "el" stands for the "element" that was just clicked
+function removeClass(el){
+	el.classList.remove("pats-silver");
+}
